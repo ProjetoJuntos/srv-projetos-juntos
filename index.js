327b087aa5d5a323bf1c9dadc8b4dd2486107116
Pedro Harmendani <pedrohnh@hotmail.com>
2022-07-07 22:06:13 -0300

    add info on log start server

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const server = require('./server');
 
 const goLive = async () => {
   const instance = await server.start();
-  console.log(`server listening on ${instance.server.address().port}`);
+  console.log(`server listening on ${instance.server.address().port} / ${process.env.BASE_ROUTE}`);
 };
 
 goLive();
