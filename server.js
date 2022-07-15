686c3e03e4718fe5536967556fe78348ac006565
Pedro Harmendani <pedrohnh@hotmail.com>
2022-07-14 22:38:50 -0300

    add cors on doadores lsit all

--- server.js
+++ server.js
@@ -6,17 +6,21 @@ const routes = require('./v1/routes');
 const server = fastify({});
 
 module.exports.start = async () => {
-  server.register(proxy, {
-    upstream: 'http://localhost:3001',
-    replyOptions: {
-      onResponse: (request, reply, res) => {
-        reply.header('Access-Control-Allow-Headers', '*');
-        reply.header('Access-Control-Allow-Origin', '*');
-        reply.header('Access-Control-Allow-Methods', '*');
-        res.send();
-      },
-    },
-  });
+  // server.register(proxy, {
+  //   upstream: `http://${app.host}:${app.port}`,
+  //   prefix: '/api',
+  //   method: 'OPTIONS',
+  //   replyOptions: {
+  //     onResponse: (request, reply, res) => {
+  //       reply.header('Access-Control-Allow-Headers', '*');
+  //       reply.header('Access-Control-Allow-Origin', `http://${app.host}:${app.port}`);
+  //       reply.header('Access-Control-Allow-Methods', '*');
+  //       reply.send(res);
+  //     },
+
+  //   },
+
+  // });
   server.register(routes, { prefix: app.baseRoute });
   await server.listen({ port: app.port, host: app.host || '0.0.0.0' });
   return server;
--- v1/controllers/DoadoresController.js
+++ v1/controllers/DoadoresController.js
@@ -12,6 +12,7 @@ const add = async (request, reply) => {
 const listAll = async (request, reply) => {
   try {
     const result = await doadoresCollection.findAll();
+    reply.header('Access-Control-Allow-Origin', '*');
     reply.code(200).send(result);
   } catch (error) {
     reply.code(error.statusCode || 500).send(error.message || error);
