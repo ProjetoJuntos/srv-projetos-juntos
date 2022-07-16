5ff471152600d3a8a75307d918bb4c8bd736760f
Pedro Harmendani <pedrohnh@hotmail.com>
2022-07-16 15:33:43 -0300

    add options route

--- v1/routes/entregadores.js
+++ v1/routes/entregadores.js
@@ -1,12 +1,23 @@
 const { add, listAll } = require('../controllers/EntregadoresController');
 
-module.exports = [{
-  method: 'GET',
-  url: '/entregadores',
-  handler: listAll,
-},
-{
-  method: 'POST',
-  url: '/entregadores',
-  handler: add,
-}];
+module.exports = [
+  {
+    method: 'OPTIONS',
+    url: '/entregadores',
+    handler(res, reply) {
+      reply.header('Content-Type', 'application/json; charset=utf-8');
+      reply.header('Access-Control-Allow-Origin', '*');
+      reply.header('Access-Control-Allow-Headers', '*');
+      reply.code(200).send();
+    },
+  },
+  {
+    method: 'GET',
+    url: '/entregadores',
+    handler: listAll,
+  },
+  {
+    method: 'POST',
+    url: '/entregadores',
+    handler: add,
+  }];
