42f53fdc78e2d7ba066c03b94e99892f84f8f9e3
Pedro Harmendani <pedrohnh@hotmail.com>
2022-07-16 15:36:46 -0300

    add doadores options route

--- v1/routes/doadores.js
+++ v1/routes/doadores.js
@@ -1,12 +1,23 @@
 const { add, listAll } = require('../controllers/DoadoresController');
 
-module.exports = [{
-  method: 'GET',
-  url: '/doadores',
-  handler: listAll,
-},
-{
-  method: 'POST',
-  url: '/doadores',
-  handler: add,
-}];
+module.exports = [
+  {
+    method: 'OPTIONS',
+    url: '/doadores',
+    handler(res, reply) {
+      reply.header('Content-Type', 'application/json; charset=utf-8');
+      reply.header('Access-Control-Allow-Origin', '*');
+      reply.header('Access-Control-Allow-Headers', '*');
+      reply.code(200).send();
+    },
+  },
+  {
+    method: 'GET',
+    url: '/doadores',
+    handler: listAll,
+  },
+  {
+    method: 'POST',
+    url: '/doadores',
+    handler: add,
+  }];
