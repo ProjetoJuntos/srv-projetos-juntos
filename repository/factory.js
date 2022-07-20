c96c1129d9cd08236cc5a863afc69d87d7dc8ab6
Pedro Harmendani <pedrohnh@hotmail.com>
2022-07-20 00:41:17 -0300

    fix loop

--- repository/factory.js
+++ repository/factory.js
@@ -29,9 +29,9 @@ class Repository {
     return result.find(filter, defaultConfigs).toArray();
   }
 
-  async findByRanking(filter = {}, sort = {}) {
+  async findByRanking({ filter = {}, sort = {} }) {
     const result = await this.db.collection(this.uri, this.dbName, this.collectionName);
-    return result.find(filter, defaultConfigs).sort(...sort).limit(5).toArray();
+    return result.find(filter, defaultConfigs).sort(sort).limit(5).toArray();
   }
 
   async updateOne({ _id }, update = {}) {
