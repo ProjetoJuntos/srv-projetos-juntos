4f40599d050e1dc117020df78a15f4c8d639441d
Pedro Harmendani <pedrohnh@hotmail.com>
2022-07-21 22:57:02 -0300

    add config jwt

--- config/index.js
+++ config/index.js
@@ -4,7 +4,7 @@ const app = () => ({
   port: process.env.PORT || 5000,
   host: process.env.HOST,
   baseRoute: process.env.BASE_ROUTE,
-  salt: process.env.SALT,
+  salt: process.env.SALT || '123500abcvvvvv',
 });
 
 const mongo = () => ({
