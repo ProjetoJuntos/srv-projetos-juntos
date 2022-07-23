dd96e70cb398fb6e8b5fea1a33316febefd9043a
Pedro Harmendani <pedrohnh@hotmail.com>
2022-07-22 22:29:10 -0300

    fix lenght array

--- v1/controllers/doacoesController.js
+++ v1/controllers/doacoesController.js
@@ -48,7 +48,7 @@ const listByCep = async (request, reply) => {
     if (cep) {
       CEP = cep.replace('-', '');
       result = await doacoesCollection.findAll({ filter: { CEP } });
-    } else if (result.lenght < 0) {
+    } else if (result.length < 0) {
       reply.code(404).send('Doações não encontrados para este Cep!');
     } else {
       reply.code(200).send(result);
