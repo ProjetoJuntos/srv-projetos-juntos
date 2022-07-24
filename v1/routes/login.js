afa5ec7a50e1ebf2b6991f80df2b6d2602028649
Pedro Harmendani <pedrohnh@hotmail.com>
2022-07-24 19:25:46 -0300

    add cors to login

--- v1/routes/login.js
+++ v1/routes/login.js
@@ -1,7 +1,17 @@
 const { login } = require('../controllers/loginController');
 
-module.exports = [{
-  method: 'POST',
-  url: '/login',
-  handler: login,
-}];
+module.exports = [
+  {
+    method: 'OPTIONS',
+    url: '/login',
+    handler(res, reply) {
+      reply.header('Content-Type', 'application/json; charset=utf-8');
+      reply.header('Access-Control-Allow-Origin', '*');
+      reply.header('Access-Control-Allow-Headers', '*');
+      reply.code(200).send();
+    },
+  }, {
+    method: 'POST',
+    url: '/login',
+    handler: login,
+  }];
